26a7d194cc982da42dd15a6552134825bdfcabd6
Fabia Rodrigues <162771932+fabidev32@users.noreply.github.com>
2024-11-29 20:34:21 -0300

    Add files via upload

---
+++ docs/processos/imagens/Conta.sql
@@ -0,0 +1,35 @@
+--Inserir Registros Conta
+
+INSERT INTO Conta (email, senha, username, idConta, idUsuario)
+VALUES
+    ('joao.silva@email.com', 'senha123', 'joaosilva', 1, 1),
+    ('maria.oliveira@email.com', 'senha456', 'mariaoliveira', 2, 2),
+    ('carlos.souza@email.com', 'senha789', 'carlossouza', 3, 3),
+    ('ana.costa@email.com', 'senha1011', 'anacosta', 4, 4),
+    ('lucas.pereira@email.com', 'senha1213', 'lucaspereira', 5, 5),
+    ('fernanda.almeida@email.com', 'senha1415', 'fernandaalmeida', 6, 6),
+    ('eduardo.santos@email.com', 'senha1617', 'eduardosantos', 7, 7),
+    ('patricia.lima@email.com', 'senha1819', 'patricialima', 8, 8),
+    ('rafael.oliveira@email.com', 'senha2021', 'rafaeloliveira', 9, 9),
+    ('juliana.rodrigues@email.com', 'senha2223', 'julianarodrigues', 10, 10),
+    ('gustavo.martins@email.com', 'senha2425', 'gustavomartins', 11, 11),
+    ('camila.barbosa@email.com', 'senha2627', 'camilabarbosa', 12, 12),
+    ('felipe.costa@email.com', 'senha2829', 'felipecosta', 13, 13),
+    ('tatiane.ferreira@email.com', 'senha3031', 'tatianeferreira', 14, 14),
+    ('marcos.souza@email.com', 'senha3233', 'marcossouza', 15, 15);
+
+--Organizações
+
+	INSERT INTO Conta (email, senha, username, idConta, idUsuario)
+VALUES
+    ('contato@naturezaviva.org', 'senha123', 'naturezaviva', 16, 16),
+    ('info@saudevida.org', 'senha456', 'saudevida', 17, 17),
+    ('contato@educartodos.org', 'senha789', 'educartodos', 18, 18),
+    ('contato@solidaria.org', 'senha1011', 'solidaria', 19, 19),
+    ('info@artecultura.org', 'senha1213', 'artecultura', 20, 20);
+
+--Representantes
+	INSERT INTO Conta (email, senha, username, idConta, idUsuario)
+VALUES
+    ('carina.aparecidaveridiano@gmail.com', 'carina123', 'CarinaQuilombola', 21, 21),
+    ('soniaguajajara@gmail.com', 'sonia123', 'SoniaLI', 22, 22);
