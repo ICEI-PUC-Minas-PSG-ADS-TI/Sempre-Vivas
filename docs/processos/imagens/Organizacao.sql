07646faf5354a65f124bef57cc6d8cf9fbeb66bb
Fabia Rodrigues <162771932+fabidev32@users.noreply.github.com>
2024-11-29 20:39:04 -0300

    Add files via upload

---
+++ docs/processos/imagens/Organizacao.sql
@@ -0,0 +1,9 @@
+--Inserir Registros Organização
+
+INSERT INTO Organizacao (nome, email, telefone, cnpj, endereco)
+VALUES 
+	('Ong Natureza Viva', 'contato@naturezaviva.org', 11987654321, 12345678901234, 'Rua Verde, 123, São Paulo, SP'),
+	('Saúde e Vida', 'info@saudevida.org', 21987654321, 23456789012345, 'Av. Saúde, 456, Rio de Janeiro, RJ'),
+	('Educar para Todos', 'contato@educartodos.org', 31987654321, 34567890123456, 'Rua Educação, 789, Belo Horizonte, MG'),
+	('Comunidade Solidária', 'contato@solidaria.org', 41987654321, 45678901234567, 'Praça Central, 101, Curitiba, PR'),
+	('Arte e Cultura', 'info@artecultura.org', 51987654321, 56789012345678, 'Rua das Artes, 202, Salvador, BA');
