15447cc564e6e8e520d3ee59d15131d8c7e4854c
Fabia Rodrigues <162771932+fabidev32@users.noreply.github.com>
2024-11-29 20:33:00 -0300

    Add files via upload

---
+++ docs/processos/imagens/Cliente.sql
@@ -0,0 +1,20 @@
+--Inserir Registros Cliente
+
+INSERT INTO Cliente (nome, endereco, telefone, dataNascimento, idConta)
+VALUES
+    ('João Silva', 'Rua A, 123, São Paulo, SP', 11987654321, '1985-04-15', 1),
+    ('Maria Oliveira', 'Av. B, 456, Rio de Janeiro, RJ', 21976543210, '1990-06-22', 2),
+    ('Carlos Souza', 'Rua C, 789, Belo Horizonte, MG', 31965432100, '1982-12-10', 3),
+    ('Ana Costa', 'Av. D, 321, Curitiba, PR', 41954321009, '1995-01-18', 4),
+    ('Lucas Pereira', 'Rua E, 234, Porto Alegre, RS', 51943210987, '1992-03-25', 5),
+    ('Fernanda Almeida', 'Rua F, 567, Recife, PE', 81932109876, '1988-08-30', 6),
+    ('Eduardo Santos', 'Rua G, 890, Salvador, BA', 71321098765, '1991-07-04', 7),
+    ('Patrícia Lima', 'Av. H, 123, Fortaleza, CE', 85987654321, '1987-11-02', 8),
+    ('Rafael Oliveira', 'Rua I, 345, Campinas, SP', 19987654321, '1993-10-15', 9),
+    ('Juliana Rodrigues', 'Av. J, 678, Manaus, AM', 92976543210, '1986-05-28', 10),
+    ('Gustavo Martins', 'Rua K, 910, Natal, RN', 84965432100, '1990-02-14', 11),
+    ('Camila Barbosa', 'Av. L, 112, Florianópolis, SC', 47954321009, '1989-09-07', 12),
+    ('Felipe Costa', 'Rua M, 334, São Luís, MA', 66943210987, '1994-12-30', 13),
+    ('Tatiane Ferreira', 'Rua N, 556, Goiânia, GO', 77932109876, '1992-11-19', 14),
+    ('Marcos Souza', 'Av. O, 778, Belém, PA', 91921098765, '1983-03-04', 15);
+
