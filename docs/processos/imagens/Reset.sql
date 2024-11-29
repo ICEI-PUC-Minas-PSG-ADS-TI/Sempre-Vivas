9135050f5fed404e2c47b4639f249d92175f5500
Fabia Rodrigues <162771932+fabidev32@users.noreply.github.com>
2024-11-29 20:41:54 -0300

    Add files via upload

---
+++ docs/processos/imagens/Reset.sql
@@ -0,0 +1,15 @@
+--Reset nas Tabelas
+
+Delete from Cliente;
+Delete from RepresentanteComunidade;
+Delete from Organizacao;
+Delete from Comunidade;
+Delete from Produto;
+Delete from Pedido;
+Delete from Realiza;
+Delete from Contem;
+Delete from Cadastra;
+Delete from Conta;
+Delete from Usuario;
+
+
