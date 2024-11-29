28defac44ecd034635da72d809eaeff9f2e08556
Fabia Rodrigues <162771932+fabidev32@users.noreply.github.com>
2024-11-29 20:41:24 -0300

    Add files via upload

---
+++ docs/processos/imagens/RepresentanteComunidade.sql
@@ -0,0 +1,12 @@
+--Inserir Registros Representante Comunidade
+
+INSERT INTO RepresentanteComunidade(nome, telefone, Papel_Social, Descricao,id_Conta)
+
+VALUES  ('Carina Aparecida Veridiano', 3199859-0912,'Liderança quilombola','Lider da Comunidade Quilombola Buieié, em Viçosa, militante da rede de Saberes dos Povos Quilombolas da Zona da Mata Sapoqui, coordenadora do Buieié Projeto Social e da Feira quilombola Buieié e diretora na Federação das Comunidades Quilombolas de Minas Gerais N´Golo. Durante sua graduação na Universidade Federal de Viçosa, publicou artigos evidenciando a luta e diversidade cultural do quilombo Buieié, além de apontar questões sobre representativa feminina, principalmente enquanto mulher negra quilombola.', 21 ),
+		('Sonia Bone de Sousa Silva Santos',6192020-1739, 'Liderança indígena', 'Ministra dos povos originários e reconhecida internacionalmente, Sonia Guajajara é do povo Guajajara/Tentehar e se destaca pelo ativismo e luta ambiental, indigena e feminista, sendo uma das 100 pessoas mais influentes do mundo. Aos 10, com pais que não sabiam ler, teve que sair de casa para trabalhar e, atualmente, enquanto coordenadora executiva da Articulação dos Povos Indígenas do Brasil, trava batalhas para conquistar o direito de povos indígenas, liderando protestos, protegendo terras e florestas da degradação e proporcionando visibilidade para as injustiças vivenciadas diariamente.', 22);
+
+
+
+
+
+
